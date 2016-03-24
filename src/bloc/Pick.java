5a994bf53ca327ffeda88010a3f3b12559dc6e95
vincent_valembois <vincent.valembois@info0729.iut-lens.univ-artois.fr>
2016-03-24 12:49:17 +0100

    Ajout ressources

---
+++ src/bloc/Pick.java
@@ -0,0 +1,14 @@
+package bloc;
+
+import javax.swing.ImageIcon;
+
+import run.MineUtils;
+
+public class Pick extends Ressource {
+
+    public Pick() {
+        super(MineUtils.PICK, 1, "ALL");
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/bloc/WoodPlanks.java
@@ -0,0 +1,12 @@
+package bloc;
+
+import javax.swing.ImageIcon;
+
+import run.MineUtils;
+
+public class WoodPlanks extends Ressource{
+
+    public WoodPlanks() {
+        super(MineUtils.WOOD_PLANKS, 5, "hache");
+    }
+}
