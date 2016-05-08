dba73ba0499a74a75f74210c8edb12c240e2f64f
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2016-05-08 20:45:12 +0200

    modification des scripts pour pouvoir compiler.

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ fi
 
 find src -name *.java -print >files.tocompile
 
-javac -cp bin -d bin @files.tocompile 
+javac -encoding ISO-8859-1 -cp bin -d bin @files.tocompile 
 cp -R src/textures bin
 jar cmf manifest.mf flatcraft.jar -C bin .
